240618ed2d18bf06ce6ab04062a5c23210a1b010
ParadineG <roman.goris@hotmail.com>
2018-10-29 17:52:35 +0200

    Maze Game Start

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    // Members
+    public float moveSpeed = 7.0f;
+    private float maxDistance = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        maxDistance += 1 * Time.deltaTime;
+        if(maxDistance >= 5)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 7.0f;
+
+    public GameObject bulletSpawner;
+    public GameObject bullet;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Player movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(bullet.transform, bulletSpawner.transform.position, bulletSpawner.transform.rotation);
+    }
+}
