9071cff76cf80a23981a517401e6efcd93227f1c
ParadineG <roman.goris@hotmail.com>
2018-11-26 17:48:44 +0200

    Better pathfinding

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 
@@ -11,21 +12,25 @@ public class Enemy : MonoBehaviour {
 
     private float passedTime = 0.0f;
     private GameObject player;
+    public NavMeshAgent agent;
 
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        
     }
     // Update is called once per frame
     void Update () {
 
+        agent.SetDestination(player.transform.position);
+        /*
         transform.LookAt(player.transform);
         passedTime += 1 * Time.deltaTime;
         if (passedTime >= 1)
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
-        
+        */
         if (health <= 0)
         {
             Die();
